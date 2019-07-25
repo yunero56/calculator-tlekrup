920aa0fbc6bb3419e512b1949cf45883472c7c81
Rachata Charoensuk [TH] <RCharoensuk@kerrylogistics.com>
2019-07-25 11:58:22 +0700

    Add function Divied

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 module.exports = {
     plus: (first, sec) => first + sec,
     subtract: (first, sec) => first - sec,
-    multiply: (first, sec) => first * sec
+    multiply: (first, sec) => first * sec,
+    divide: (first,sec) => first / sec
 }
--- test.js
+++ test.js
@@ -2,4 +2,4 @@ const cal = require('./index');
 console.log(cal.plus(1,2));
 console.log(cal.subtract(2,1));
 console.log(cal.multiply(5,2));
-
+console.log(cal.divide(1000,2));
